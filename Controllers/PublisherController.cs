42f0dfb561a9a4c87b94dbe728c7889dc9d7b474
Ritika <ritika.agr7688@gmail.com>
2024-01-08 18:16:40 +0530

    Initial commit

---
+++ Controllers/PublisherController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineBookStore.Models.Domain;
+using OnlineBookStore.Repositories.Abstract;
+
+namespace OnlineBookStore.Controllers
+{
+    public class PublisherController : Controller
+    {
+        private readonly IPublisherService service;
+        public PublisherController(IPublisherService service)
+        {
+            this.service = service;
+        }
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(Publisher model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            var result = service.Add(model);
+            if (result)
+            {
+                TempData["message"] = "Added Successfully";
+                return RedirectToAction(nameof(Add));
+            }
+            TempData["message"] = "Error has occured on server side";
+            return View(model);
+        }
+
+
+        public IActionResult Update(int id)
+        {
+            var record = service.FindById(id);
+            return View(record);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Publisher model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            var result = service.Update(model);
+            if (result)
+            {
+                return RedirectToAction("GetAll");
+            }
+            TempData["message"] = "Error has occured on server side";
+            return View(model);
+        }
+
+
+        public IActionResult Delete(int id)
+        {
+
+            var result = service.Delete(id);
+            return RedirectToAction("GetAll");
+        }
+
+        public IActionResult GetAll()
+        {
+
+            var data = service.GetAll();
+            return View(data);
+        }
+
+    }
+}
